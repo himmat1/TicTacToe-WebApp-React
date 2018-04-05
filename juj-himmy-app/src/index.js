48618848eea61062b78d73206791ae46691d92fb
Himmat  Singh <himmatsingh@Himmats-MacBook-Pro.local>
2018-04-05 01:04:27 -0700

    finished the tic tac toe basic functionality game

--- juj-himmy-app/src/index.js
+++ juj-himmy-app/src/index.js
@@ -53,7 +53,12 @@ class Board extends React.Component {
         //2. Tracking changes
         //3. Determing when to re-render in REact
       const squares = this.state.squares.slice(); 
-      squares[i] = 'X'; 
+
+      //code that handles no clicking if someone has already won the game
+      if(calculateWinner(squares)||squares[i]){
+        return; 
+      }
+      squares[i] = this.state.playerOneIsNext ? 'X': 'O'; 
       this.setState({
         squares:squares, 
         playerOneIsNext: !this.state.playerOneIsNext, 
@@ -71,8 +76,14 @@ class Board extends React.Component {
 
 
   render() {
-    const status = 'Next player: ' + (this.state.playerOneIsNext ? 'X': 'O');
-
+    //const status = 'Next player: ' + (this.state.playerOneIsNext ? 'X': 'O');
+    const winner = calculateWinner(this.state.squares); 
+    let status; 
+    if(winner){
+      status = 'Winner: '+ winner; 
+    } else {
+      status = 'Next player: ' + (this.state.playerOneIsNext ? 'X' : 'O'); 
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -114,7 +125,25 @@ class Game extends React.Component {
   }
 }
 
-
+function calculateWinner(squares){
+  const lines = [
+      [0, 1, 2], 
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for(let i = 0; i < lines.length; i++){
+      const[a,b,c] = lines[i]; 
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+        return squares[a]; 
+      }
+    }
+    return null;
+}
 
 
 // ========================================
